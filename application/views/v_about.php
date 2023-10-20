2236f9b3652143b0abb2cb35beaf22276707e892
zalditri10 <zalditrighifari@gmail.com>
2023-10-20 15:27:40 +0700

    menambahkan modified

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -1,8 +1,7 @@
 <section>
 <center><h1><?php echo $judul ?></h1></center>
- <center><img src="<?php echo base_url()?>assets/img/Foto.jpg" width = "300px" height ="400px"></center>
  <ul type="disc">
-    <h3 align ="center">Nama : ZaldiTri Ghifari</h3>    
+    <h3 align ="center">Nama : ZaldiTri Ghifari</h3>
 </ul>
 <ul type="none">
     <h3 align = "center"> Alamat: Jl.Kenanga XII Pondok Ungu Permai Blok C 26/22 RT 003 RW 010 </h3>
@@ -13,4 +12,5 @@
 </ul>
 
 <h3 align ="center">Olah Raga Favorit : Berenang Dan Badminton</h3>
+<center><img src="<?php echo base_url()?>assets/img/Foto.jpg" width = "300px" height ="400px"></center>
 </section>
