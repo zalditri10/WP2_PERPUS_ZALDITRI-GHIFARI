6811e8f03e0b40fc773414a0c674be90d5768535
zalditri10 <zalditrighifari@gmail.com>
2023-10-20 14:35:27 +0700

    menambahkan foto

---
+++ application/views/v_about.php
@@ -0,0 +1,16 @@
+<section>
+<center><h1><?php echo $judul ?></h1></center>
+ <center><img src="<?php echo base_url()?>assets/img/Foto.jpg" width = "300px" height ="400px"></center>
+ <ul type="disc">
+    <h3 align ="center">Nama : ZaldiTri Ghifari</h3>    
+</ul>
+<ul type="none">
+    <h3 align = "center"> Alamat: Jl.Kenanga XII Pondok Ungu Permai Blok C 26/22 RT 003 RW 010 </h3>
+</ul>
+</ul>
+<ul type="none">
+    <h3 align = "center">Tempat Lahir : Jakarta, 10 Juni 2004</h3>
+</ul>
+
+<h3 align ="center">Olah Raga Favorit : Berenang Dan Badminton</h3>
+</section>
